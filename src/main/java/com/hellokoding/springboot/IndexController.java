ab69ce02b1ebf71ec7ce6a51e2f7d7541ffaf2ae
Liu Dake <lolitacon2012@gmail.com>
2017-06-10 12:40:06 +0800

    First commit from Ubuntu@home

---
+++ src/main/java/com/hellokoding/springboot/IndexController.java
@@ -0,0 +1,16 @@
+package com.hellokoding.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+    public String index(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);//add jsp field variable
+        return "index";//page name to return
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Hello</title>
+    <script>alert(${name});</script>
 </head>
 <body>
     Hello ${name}
